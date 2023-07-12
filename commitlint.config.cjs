64111f031b6beec37e926804c22ebedc67914ea3
rickon-DAFEI <wangyu12@accesscorporate.com.cn>
2023-07-12 13:30:41 +0800

    测试

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,29 +1,35 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  // 校验规则
   rules: {
     'type-enum': [
       2,
       'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-        'revert',
-        'build',
-      ],
+      ['feat', 'fix', 'docs', 'refactor', 'style', 'test'],
     ],
+    'header-max-length': [2, 'always', 72],
+    'subject-case': [0],
+    'subject-empty': [0],
+    'scope-empty': [0],
     'type-case': [0],
     'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'refactor', 'style', 'test'],
+    ],
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 72],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 72],
     'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72],
+    'subject-min-length': [0],
+    'subject-max-length': [0],
   },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w+)\(([\w-]+)\):\s(.+)$/,
+      headerCorrespondence: ['type', 'component', 'commitWord'],
+    },
+  },
+  formatter: '@commitlint/format',
 }
